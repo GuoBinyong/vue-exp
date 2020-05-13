c2257056319da1c401ca71e79ea0a3bba183b487
郭斌勇 <guobinyong@qq.com>
2020-05-13 11:47:14 +0800

    移除合并策略到vue-tools

--- dist/vue-exp.umd.js
+++ dist/vue-exp.umd.js
@@ -57,38 +57,9 @@
     } else {
       throw "regMixins 只能接收数组类型的参数";
     }
-  }; //合并策略：开始
-
-
-  Vue.byMergeStrategies = {
-    /**
-     * 合并的结果会按顺序包含 parent 和 child ;
-     * @param parent
-     * @param child
-     * @param vm
-     * @returns [parent,child]
-      注意：
-     Vue 的 合并策略 mergeHook 有个bug，原码如下：
-      // Hooks and props are merged as arrays.
-     function mergeHook (
-     parentVal,
-     childVal
-     ) {
-    return childVal
-      ? parentVal
-        ? parentVal.concat(childVal)  //这里应该先判断 parentVal 是否是数组
-        : Array.isArray(childVal)
-          ? childVal
-          : [childVal]
-      : parentVal
-    }
-     */
-    includeAllWihtArray: function includeAllWihtArray(parentVal, childVal, vm) {
-      return childVal ? parentVal ? Array.isArray(parentVal) ? parentVal.concat(childVal) : [parentVal].concat(childVal) : Array.isArray(childVal) ? childVal : [childVal] : parentVal;
-    }
-  }; //合并策略：结束
-  //混合：开始
+  }; //混合：开始
   //vnode相关的混合
+
 
   var vnodeMixin = {
     computed: {
